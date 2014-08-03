5ba368c6e9ae455efa0b74f89fac9594c49137b1
Frank Lemanschik <frank@dspeed.eu>
2014-08-03 17:38:02 +0200

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
 Implementation
 
 I require a php script that is capable of doing this. (plus a configured .htaccess file that allows me write redirect.com/move/YmxvZy5jb20vYXJ0aWNsZS9h and still target the php script )
@@ -17,33 +17,39 @@ b. for every image inside ImageList => it will output an image tage with the ima
 // Getting Parameters from rewrite or directly
 $url = base64_decode($_GET[url]);
 //read the entire string
-$output=file_get_contents('template.html');
-
+$output_html=file_get_contents('template.html');
+$output_js=file_get_contents('geoip-params.js');
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 $output_remote = curl_exec($curl);
 curl_close($curl);
 
 $DOM = new DOMDocument;
+libxml_use_internal_errors(true);
 $DOM->loadHTML( $output_remote);
+libxml_clear_errors();
+
 
 //get all img tags and title tags into vars
 $itemstitle = $DOM->getElementsByTagName('title');
 $itemsimg = $DOM->getElementsByTagName('img');
 
  for ($i = 0; $i < $itemstitle->length; $i++)
-        $inserttitle =  $itemstitle->item($i)->nodeValue;
+        $inserttitle = $itemstitle->item($i)->nodeValue;
 
 //display all H1 text
  $insertimgs = "";
- for ($i = 0; $i < $itemsimg->length; $i++)
-        $insertimgs .= $itemsimg->item($i)->nodeValue . "<br/>";
+ for ($i = 0; $i < $itemsimg->length; $i++) {
+        $insertimgs .= '<img src="'.$itemsimg->item($i)->getAttribute('src') . '" /> <br/>';
+    }
 
 // CREATING dom object from url and extract titel and imags in loop and save into vars
 
 //replace something in the string - this is a VERY simple example
-$output=str_replace("###TITLE###", $inserttitle, $output);
-$output=str_replace("###IMGS###", $insertimgs, $output);
-echo $output;
+$output_html=str_replace("###TITLE###", $inserttitle, $output_html);
+$output_html=str_replace("###IMGS###", $insertimgs, $output_html);
+$output_js=str_replace("###URL###", $url, $output_js);
+$output_html=str_replace("###JS###", $output_js, $output_html);
+echo $output_html;
 
 // Loadtemplate and insert Title and Images then return it
